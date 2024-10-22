035252bb07403b4d731e2b49bef8d4041b44e7ce
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-10-22 18:31:17 -0500

    update licenses, min sphinx

--- recipe/collect_licenses.py
+++ recipe/collect_licenses.py
@@ -2,27 +2,30 @@ import shutil
 from pathlib import Path
 import os
 import tempfile
+import sys
 
 SRC_DIR = Path(os.environ["SRC_DIR"])
 PKG_VERSION = os.environ["PKG_VERSION"]
 
 WHL = SRC_DIR / f"pydata_sphinx_theme-{PKG_VERSION}-py3-none-any.whl"
 
-LICENSE_DIR = SRC_DIR / "third-party-licenses"
-LICENSE_DIR.mkdir()
 
-LICENSES = {
-    "font-awesome-LICENSE.txt": "pydata_sphinx_theme/theme/pydata_sphinx_theme/static/vendor/fontawesome/*/LICENSE.txt"
-}
+def main() -> int:
+    license_dir = SRC_DIR / "third-party-licenses"
+    license_dir.mkdir()
 
-with tempfile.TemporaryDirectory() as td:
+    with tempfile.TemporaryDirectory() as td:
+        tdp = Path(td)
+        ZIP = tdp / f"{WHL.name}.zip"
+        DIST = tdp / "dist"
+        shutil.copy2(WHL, ZIP)
+        shutil.unpack_archive(ZIP, DIST)
 
-    tdp = Path(td)
-    ZIP = tdp / f"{WHL.name}.zip"
-    DIST = tdp / "dist"
-    shutil.copy2(WHL, ZIP)
-    shutil.unpack_archive(ZIP, DIST)
+        for license in DIST.glob("pydata_sphinx_theme/static/*LICENSE.*"):
+            shutil.copy2(license, license_dir / license.name)
 
-    for dest, pattern in LICENSES.items():
-        license = next(DIST.glob(pattern))
-        shutil.copy2(license, LICENSE_DIR / dest)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
